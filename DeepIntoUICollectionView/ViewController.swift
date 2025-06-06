7da6d4189d937a228bd22a886492f1ac33259cf1
hummanxo <rxkolchin@gmail.com>
2025-06-06 15:44:57 +0300

    Initial Commit

---
+++ DeepIntoUICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeepIntoUICollectionView
+//
+//  Created by Aleksandr on 06.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
